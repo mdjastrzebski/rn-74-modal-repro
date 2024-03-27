d458f71550e2aacc879141533dc3bb0004ce73e2
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-03-27 15:52:34 +0100

    chore: add failing test

--- ReproducerApp/__tests__/App.test.tsx
+++ ReproducerApp/__tests__/App.test.tsx
@@ -2,7 +2,7 @@
  * @format
  */
 
-import 'react-native';
+import {Modal, View} from 'react-native';
 import React from 'react';
 import App from '../App';
 
@@ -14,4 +14,16 @@ import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('renders Modal correctly', () => {
+  renderer.create(<Modal />);
+});
+
+it('renders Modal with children correctly', () => {
+  renderer.create(
+    <Modal visible={true}>
+      <View />
+    </Modal>,
+  );
 });
